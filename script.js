1f446b8087646b72b48bb8b553557019831178f7
Chris <cher1262@gmail.com>
2020-08-09 03:35:54 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let colors = ["#eb4888", "#10a2f5", "#e9bc3f"];
+
+(function() {
+  setModeEventListener();
+  setBioEventListener();
+})();
+
+/* Dark Mode */
+function setModeEventListener() {
+  let list = document.body.classList;
+  document.getElementById("toggler").addEventListener("change", event => {
+    event.target.checked ? list.add("dark-mode") : list.remove("dark-mode");
+  });
+}
+
+/* Bio Toggles */
+
+function setBioEventListener() {
+  Array.from(document.getElementsByTagName("button")).forEach(e => {
+    e.addEventListener("click", bioToggle);
+  });
+}
+
+function bioToggle(e) {
+  let bioType = e.target;
+  let color = "#F2A365";
+  off(bioType);
+  bioType.style.cssText = `border-color: ${color}; color: ${color}; font-weight: bold;`;
+  let bioTypeElement = document.getElementsByClassName(bioType.id)[0];
+  if (bioTypeElement !== undefined) bioTypeElement.classList.add("show");
+}
+
+function off(bioType) {
+  Array.from(document.getElementsByTagName("button")).forEach(butt => {
+    butt.style.borderColor = "#96979c";
+    butt.style.color = "#96979c";
+  });
+  Array.from(document.getElementsByClassName("bio")).forEach(e => {
+    e.classList.remove("show");
+  });
+}
